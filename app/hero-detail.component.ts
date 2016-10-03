2b21e7e31964a9ce7192804e0b43badffaaf5d0f
Alexander Bestcenny <bestic@gmail.com>
2016-10-03 15:10:44 +0300

    first commit

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
+import {ActivatedRoute, Params} from '@angular/router';
+
+@Component({
+  selector: 'my-hero-detail',
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'];
+      this.heroService.getHero(id).then(hero => this.hero = hero);
+    });
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(this.goBack)
+  }
+
+  goBack(): void {
+    window.history.back();
+  }
+
+  hero: Hero;
+}
